22dd09f7b2ac11b2388e07923c46cf530a249910
Makayla Murphy <makayla_murphy@brown.edu>
2019-06-12 23:32:47 -0500

    Initial Commit

---
+++ Emojional/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojional
+//
+//  Created by Makayla Murphy on 6/12/19.
+//  Copyright © 2019 Makayla Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
