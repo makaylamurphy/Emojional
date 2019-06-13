64e980d475a27af772472960c31745ecd156aa51
Makayla Murphy <makayla_murphy@brown.edu>
2019-06-13 00:32:48 -0500

    Complete Iteration 2

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let emojis = ["😃": "Be happy!", "😞": "I'm sorry you're sad!"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +22,9 @@ class ViewController: UIViewController {
         // when a user clicks an emoji, there needs to be a pop-up
         // that show "we all get EMOJInal" and a message in-line with the emoji
         
-        let alertContoller = UIAlertController(title: "alert title", message: "alert message", preferredStyle: UIAlertController.Style.alert)
+        let selectedEmotion = String(sender.titleLabel!.text!)
+        
+        let alertContoller = UIAlertController(title: "Welcome to EMOJIonal", message: emojis[selectedEmotion]!, preferredStyle: UIAlertController.Style.alert)
         alertContoller.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertContoller, animated: true, completion: nil)
         
